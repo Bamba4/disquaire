e8c1f54032b124b1e7421d0cf83ed8f7b40d1c57
Bamba <bamba4>
2021-02-03 17:30:02 +0000

    first commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cheikhahmadoubambadiagne/Desktop/dmdb-backend/env"
+set -gx VIRTUAL_ENV "/Users/cheikhahmadoubambadiagne/Desktop/disquaire_project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
